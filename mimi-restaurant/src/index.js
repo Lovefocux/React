86d78c095bf3d5ead95182483d21b255cb815910
Joshua Olabode <olabodejoshuaf@gmail.com>
2023-03-09 02:47:44 +0100

    src folder and index.js file created

---
+++ mimi-restaurant/src/index.js
@@ -0,0 +1,8 @@
+// This connect the app to the DOM
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+import App from './App';
+
+// Render this app under document GetElementId of Root
+ReactDOM.render(<App />, document.getElementById('root'));
